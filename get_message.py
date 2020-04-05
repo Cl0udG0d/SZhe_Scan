84e76749947482b96159693fa62a20a4f1ab1e53
潘一 <2585614464@qq.com>
2020-04-05 19:08:38 +0800

    子域名模块编写

--- get_message.py
+++ get_message.py
@@ -27,10 +27,29 @@ def get_whois(domain):
 
 '''
 每个域名的情况都不一样，，比如一个被爬虫收录很差的域名，我们采用搜索引擎搜索的话很难搜集到全部的子域名
-这里被动子域名
+    这里使用在线网站搜集和必应搜索引擎搜集两种搜集方式实现被动子域名搜集
+    在线子域名搜集：https://tool.chinaz.com/subdomain
+    必应搜索引擎搜集：https://cn.bing.com/
 '''
 def get_sundomain(domain):
+
+    base_url='https://tool.chinaz.com/'
+    chinaz_url = 'https://tool.chinaz.com/subdomain?domain=' + domain + '&page=1'
+    context=[]
+    while 1:
+        rep = requests.get(chinaz_url)
+        rep = etree.HTML(rep.text)
+        data = rep.xpath('//div[@class="w23-0"]/a[@href="javascript:"]/text()')
+        str = "\n".join(data)
+        print(str)
+        try:
+            next_url = rep.xpath('//a[@title="下一页"]/@href')[0]
+            chinaz_url=base_url+next_url
+        except:
+            break
+
 
 
 #测试数据
 # get_whois("shkls.com")
+get_sundomain("baidu.com")
