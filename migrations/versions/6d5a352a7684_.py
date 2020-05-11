eaf28a956f9e003c43a494b470739aa812be7e0f
cheerddd <2664330029@qq.com>
2020-05-12 00:36:40 +0800

    控制台/漏洞列表/个人中心页面修改+Chart.js/echarts.js文件引入

--- migrations/versions/6d5a352a7684_.py
+++ migrations/versions/6d5a352a7684_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f332244bdfc3
+Revision ID: 6d5a352a7684
 Revises: 
-Create Date: 2020-05-11 17:50:51.396727
+Create Date: 2020-05-11 20:40:29.654941
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f332244bdfc3'
+revision = '6d5a352a7684'
 down_revision = None
 branch_labels = None
 depends_on = None
