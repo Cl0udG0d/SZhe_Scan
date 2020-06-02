b53fb705cd62556ff35934752abf66e1315baff5
cheerddd <2664330029@qq.com>
2020-06-02 17:23:54 +0800

    页码修改/控制台新建任务修改/poc页面修改/个人中心资产修改

--- static/js/home.js
+++ static/js/home.js
@@ -132,3 +132,56 @@ $(".skill-per").each(function () {
     }
   );
 });
+
+//页码部分
+$(function () {
+  var currentPage = Number("{{paginate.page}}"); //当前页
+  var pageNum = Number("{{paginate.pages}}"); //总页数
+
+  $("#btn2").text(currentPage - 2);
+  $("#btn3").text(currentPage - 1);
+  $("#btn4").text(currentPage);
+  $("#btn5").text(currentPage + 1);
+  $("#btn6").text(currentPage + 2);
+  $("#btn7").text(pageNum);
+
+  $("#btn4").css("color", "#4f90fb");
+  $("#btn4").css("border-color", "#4f90fb");
+
+  if (currentPage == 1) {
+    $("#prePage").hide();
+  }
+
+  if (currentPage == pageNum) {
+    $("#sufPage").hide();
+  }
+
+  if (currentPage <= 3) {
+    $("#prePoint").hide();
+    $("#btn1").hide();
+  } else if (currentPage == 4) {
+    $("#prePoint").hide();
+  }
+  if (currentPage == 1) {
+    $("#btn2").hide();
+    $("#btn3").hide();
+  } else if (currentPage == 2) {
+    $("#btn2").hide();
+  }
+
+  if (currentPage >= pageNum - 2) {
+    $("#sufPoint").hide();
+    $("#btn7").hide();
+  } else if (currentPage == pageNum - 3) {
+    $("#sufPoint").hide();
+  }
+
+  if (currentPage == pageNum) {
+    $("#btn5").hide();
+    $("#btn6").hide();
+  }
+
+  if (currentPage == pageNum - 1) {
+    $("#btn6").hide();
+  }
+});
--- static/js/setting.js
+++
@@ -1,3 +0,0 @@
-$("#item1").click(function () {
-  console.log($(this).prop("checked"));
-});
