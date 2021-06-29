2a681c72da4a608f98c91a513db4df09f50a0f0a
Cl0udG0d <2585614464@qq.com>
2021-06-30 00:52:49 +0800

    前端界面修改

--- app/api/admin/index.py
+++ app/api/admin/index.py
@@ -4,7 +4,11 @@ from flask import render_template
 
 @admin.route('/a')
 def adminIndex():
-    return render_template("index.html")
+    return render_template("index_v3.html")
+
+@admin.route('/test')
+def test():
+    return render_template("test.html")
 
 @admin.route('/')
 def base():
