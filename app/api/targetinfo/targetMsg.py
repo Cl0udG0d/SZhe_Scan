15284de244bdf7fb90c00c96fcd671dd3ae36fd6
Cl0udG0d <2585614464@qq.com>
2021-07-09 00:11:35 +0800

    add little code

--- app/api/targetinfo/targetMsg.py
+++ app/api/targetinfo/targetMsg.py
@@ -1,7 +1,27 @@
-
+from app.utils.selfrequests import normalReq
 
 class TargetMsg:
-    def __init__(self):
+    def __init__(self,url):
+        self.url=url
+        self.domain=self.getDomain()
+        self.target=self.getTarget()
+        self.isDomain=self.checkDomain()
+        self.rep=None
+
+        return
+
+    def getTarget(self):
+        self.rep=normalReq(self.target)
+
+    def getDomain(self):
+        return
+
+    def getResponse(self):
+
+        return
+
+    def getStatus(self):
+
         return
 
     def getTargetPort(self):
@@ -9,3 +29,6 @@ class TargetMsg:
 
     def getTargetSensitiveMsg(self):
         return
+
+    def checkDomain(self):
+        pass
--- app/utils/selfrequests.py
+++ app/utils/selfrequests.py
@@ -1,6 +1,9 @@
-import httpx
+import requests
+from app.utils import randomAgent
 
-
-
-def req():
-    return
+def normalReq(url,timeout=5,verify=False,allow_redirects=False):
+    try:
+        rep=requests.get(url,timeout=timeout,verify=verify,allow_redirects=allow_redirects,headers=randomAgent.getRandomUserAgent())
+        return rep
+    except:
+        raise ("ERROR:{}请求超时".format(url))
