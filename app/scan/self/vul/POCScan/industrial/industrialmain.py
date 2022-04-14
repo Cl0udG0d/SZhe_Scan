557d50d8260e6c2aff093d298cef07fef3a992c5
Cl0udG0d <2585614464@qq.com>
2022-04-14 11:38:23 +0800

    删除内置冗余poc,检测pip依赖

--- app/scan/self/vul/POCScan/industrial/industrialmain.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-name: 工业控制漏洞库
-referer: unknow
-author: Lucifer
-description: 包含所有industrial control漏洞类型，封装成一个模块
-'''
-#wireless
--- app/scan/self/vul/POCScan/information/informationmain.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-name: information漏洞库
-referer: unknow
-author: Lucifer
-description: 包含所有information漏洞类型，封装成一个模块
-'''
